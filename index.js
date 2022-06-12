4e4036aba4cac1129a1211bf29ae8d529d710bf5
3165254905 <runfeng.yu@foxmail.com>
2022-06-12 10:34:45 +0800

    22061210340

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -20,9 +20,30 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get("/api/:date?", function (req, res) {
+  if (req.params.date == null) {
+    res.json({
+      unix: new Date().getTime(),
+      utc: new Date().toUTCString()
+    })
+  } else if (req.params.date.includes("-")) {
+    if (new Date(req.params.date).toUTCString() === "Invalid Date") {
+      res.json({
+        error: "Invalid Date"
+      })
+    } else {
+      res.json({
+        unix: new Date(req.params.date).getTime(),
+        utc: new Date(req.params.date).toUTCString()
+      })
+    }
+  } else {
+    res.json({
+      unix: req.params.date,
+      utc: new Date(req.params.date * 1000).toUTCString(),
+    })
+  }
+})
 
 
 
